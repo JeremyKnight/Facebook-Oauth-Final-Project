2f7d8f7d01620c775c12640f92ac4a0ac9742e50
jaredfitton <jaredfitton@gmail.com>
2018-04-12 10:14:41 -0700

    Completed Remote App

--- webapp.py
+++ webapp.py
@@ -2,16 +2,20 @@ from flask import Flask, url_for, render_template, request
 from flask_oauthlib.client import OAuth
 from flask import render_template
 
+import os
+
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-facebook = oauth.remote_app('facebook',
+oauth = OAuth()
+
+facebook = oauth.remote_app(
+    'facebook',
     base_url='https://graph.facebook.com/',
     request_token_url=None,
     access_token_url='/oauth/access_token',
     authorize_url='https://www.facebook.com/dialog/oauth',
     consumer_key= os.environ['FACEBOOK_APP_ID'],
-    consumer_secret= os.environ['FACEBOOK_APP_SECRET'],
-    request_token_params={'scope': 'email'}
+    consumer_secret= os.environ['FACEBOOK_APP_SECRET']
 )
 
 @app.route("/")
