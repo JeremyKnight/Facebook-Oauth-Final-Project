5a918f6e3add415763b428eed95589a377a60ae5
jaredfitton <jaredfitton@gmail.com>
2018-04-12 09:48:07 -0700

    Added facebook oauth conenction

--- webapp.py
+++ webapp.py
@@ -4,9 +4,20 @@ from flask import render_template
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key= os.environ['FACEBOOK_APP_ID'],
+    consumer_secret= os.environ['FACEBOOK_APP_SECRET'],
+    request_token_params={'scope': 'email'}
+    access_token_method='POST',
+)
+
 @app.route("/")
 def render_main():
     return render_template('home.html')
 
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=False)
