531b4870538ed67e5e1030c8bbf89436ea42cd4b
jaredfitton <jaredfitton@gmail.com>
2018-04-13 09:41:52 -0700

    Merged prev commit

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
 export FACEBOOK_APP_ID=874728239365777
 export FACEBOOK_APP_SECRET=1ba882993f562e82ecaa63edc726cd21
+export SECRET_KEY="B\xd0\x9b\x8dwr\xeb~\xc1ZK'\xa1\xa0\xf3\x83\x87\x86n\xb6\x12\x9c}\xdd"
--- webapp.py
+++ webapp.py
@@ -2,19 +2,22 @@ from flask import Flask, url_for, render_template, request, session
 from flask_oauthlib.client import OAuth
 from flask import render_template
 
+import os
+
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 
+oauth = OAuth()
 
-facebook = oauth.remote_app('facebook',
+facebook = oauth.remote_app(
+    'facebook',
     base_url='https://graph.facebook.com/',
     request_token_url=None,
     access_token_url='/oauth/access_token',
     authorize_url='https://www.facebook.com/dialog/oauth',
     consumer_key= os.environ['FACEBOOK_APP_ID'],
-    consumer_secret= os.environ['FACEBOOK_APP_SECRET'],
-    request_token_params={'scope': 'email'}
+    consumer_secret= os.environ['FACEBOOK_APP_SECRET']
 )
 
 @app.route("/")
@@ -43,10 +46,10 @@ def authorized(resp):
     flash('You were signed in as %s' % resp['screen_name'])
     return redirect(next_url)
 
-''''resp = github.authorized_response()
+'''resp = github.authorized_response()
    if resp is None:
         session.clear()
-        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)
     else:
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
@@ -56,7 +59,7 @@ def authorized(resp):
             session.clear()
             print(inst)
             message='Unable to login, please try again.  '
-    return render_template('message.html', message=message)''''
+    return render_template('message.html', message=message)'''
 
 if __name__=="__main__":
     app.run(debug=False)
