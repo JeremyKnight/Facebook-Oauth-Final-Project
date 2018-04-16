dc205b2fb2e1718f645b1237e7918e5f4e7d0be9
jeremyknight <jeremyknightjck99@gmail.com>
2018-04-16 09:47:10 -0700

    added mongo stuff for mlab

--- env.sh
+++ env.sh
@@ -1,3 +1,8 @@
 export FACEBOOK_APP_ID=874728239365777
 export FACEBOOK_APP_SECRET=1ba882993f562e82ecaa63edc726cd21
 export SECRET_KEY="B\xd0\x9b\x8dwr\xeb~\xc1ZK'\xa1\xa0\xf3\x83\x87\x86n\xb6\x12\x9c}\xdd"
+export MONGO_DBNAME=facebook-oauth
+export MONGO_HOST=ds013495.mlab.com
+export MONGO_USERNAME=dbuser1
+export MONGO_PASSWORD=password
+export MONGO_PORT=13495
