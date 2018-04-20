4a78dc4dafc6a8e41dab4ea4f5b647c5bc197fe7
jaredfitton <jaredfitton@gmail.com>
2018-04-20 09:16:36 -0700

    Removed print profile

--- webapp.py
+++ webapp.py
@@ -48,10 +48,10 @@ def facebook_callback():
         print("You denied the request to sign in.")
 
     session['facebook_oauth_token'] = access_token
-    # 
+    #
     # graph = facebook.GraphAPI(access_token)
     # profile = graph.get_object("me")
-    print(profile)
+    # print(profile)
 
 # @app.route('/authorized')
 # def authorized():
